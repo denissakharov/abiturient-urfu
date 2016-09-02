e31251f6b51be21718c6c003cb6c511ddd4b3262
Denis Sakharov <denis.sakharov@hotmail.com>
2016-09-02 19:12:29 +0500

    chore(meta): add general meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(title)
+    base = 'Список поступающих в УрФУ'
+    if title.empty?
+      base
+    else
+      "#{title} | #{base}"
+    end
+  end
 end
